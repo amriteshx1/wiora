6d7d74c78a3e1036c36ee07de281b3ae022c3d39
amriteshx1 <amriteshx18@gmail.com>
2025-09-08 15:06:35 +0530

    switch default font from Geist to Inter and updated auth ui's radial gradient

--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -64,7 +64,7 @@ export const SignInView = () => {
     };
 
     return (
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 font-sans">
             <Card className="overflow-hidden p-0">
                 <CardContent className="grid p-0 md:grid-cols-2">
                     <Form {...form}>
@@ -163,7 +163,7 @@ export const SignInView = () => {
                         </form>
                     </Form>
 
-                    <div className="bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+                    <div className="bg-radial from-neutral-800 to-neutral-950 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
                         <img src="/logo.svg" alt="Image" className="h-[92px] w-[92px]" />
                         <p className="text-2xl font-semibold text-white">
                             Wiora
