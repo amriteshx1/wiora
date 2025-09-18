a9bc0d9e8aa68d108c991880ce9f43a593d6f0c5
amriteshx1 <amriteshx18@gmail.com>
2025-09-18 19:27:34 +0530

    add AgentsView with query integration and state handling

---
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+
+import { useTRPC } from "@/trpc/client";
+import { ErrorState } from "@/components/error-state";
+import { LoadingState } from "@/components/loading-state";
+
+export const AgentsView = () => {
+    const trpc = useTRPC();
+    const { data, isLoading, isError } = useQuery(trpc.agents.getMany.queryOptions());
+
+    if(isLoading){
+        return (
+            <LoadingState 
+              title="Loading Agents"
+              description="This may take a few seconds"
+            />
+        )
+    };
+
+    if(isError){
+        return(
+            <ErrorState
+              title="Error Loading Agents"
+              description="Please try again later"
+            />
+        )
+    };
+    
+    return (
+        <div>
+            {JSON.stringify(data, null, 2)}
+        </div>
+    );
+};
