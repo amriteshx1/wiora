fc3909f304510ab4d02b33cb5f997036705df9a5
amriteshx1 <amriteshx18@gmail.com>
2025-09-22 19:51:32 +0530

    add create agent mutation with protectedProcedure in agentsRouter

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,6 +1,8 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { createTRPCRouter, baseProcedure } from "@/trpc/init";
+import { createTRPCRouter, baseProcedure, protectedProcedure } from "@/trpc/init";
+
+import { agentsInsertSchema } from "../schemas";
 
 export const agentsRouter = createTRPCRouter({
     getMany: baseProcedure.query( async () => {
@@ -10,4 +12,17 @@ export const agentsRouter = createTRPCRouter({
 
         return data;
     }),
+    create: protectedProcedure
+      .input(agentsInsertSchema)
+      .mutation(async ({ input, ctx }) => {
+        const [createdAgent] = await db
+          .insert(agents)
+          .values({
+            ...input,
+            userId: ctx.auth.user.id,
+          })
+          .returning();
+
+          return createdAgent;
+      }),
 });
