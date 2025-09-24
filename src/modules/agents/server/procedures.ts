8aaf1c126ba2891b8344d9fa0dba5270db745bff
amriteshx1 <amriteshx18@gmail.com>
2025-09-24 20:45:37 +0530

    include dummy meetingCount field in getOne query

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,6 +1,6 @@
 import {z} from "zod";
 
-import { eq } from "drizzle-orm";
+import { eq, getTableColumns, sql } from "drizzle-orm";
 
 import { db } from "@/db";
 import { agents } from "@/db/schema";
@@ -11,7 +11,10 @@ import { agentsInsertSchema } from "../schemas";
 export const agentsRouter = createTRPCRouter({
     getOne: protectedProcedure.input(z.object({ id: z.string() })).query( async ({ input }) => {
         const [existingAgent] = await db
-          .select()
+          .select({
+            meetingCount: sql<number>`5`,
+            ...getTableColumns(agents),
+          })
           .from(agents)
           .where(eq(agents.id, input.id))
 
