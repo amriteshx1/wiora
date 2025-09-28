bf892bae92f1fd3d6bddd07cd08260a04d93bb05
amriteshx1 <amriteshx18@gmail.com>
2025-09-28 18:15:42 +0530

    add horizontal ScrollArea and ScrollBar to AgentsListHeader

--- src/modules/agents/ui/components/agents-list-header.tsx
+++ src/modules/agents/ui/components/agents-list-header.tsx
@@ -9,6 +9,7 @@ import { NewAgentDialog } from "./new-agent-dialog";
 import { useAgentsFilters } from "../../hooks/use-agents-filters";
 import { AgentsSearchFilter } from "./agents-search-filter";
 import { DEFAULT_PAGE } from "@/constants";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 export const AgentsListHeader = () => {
     const [filters, setFilters] = useAgentsFilters();
@@ -34,6 +35,7 @@ export const AgentsListHeader = () => {
                     New Agent
                 </Button>
             </div>
+            <ScrollArea>
             <div className="flex items-center gap-x-2 p-1">
               <AgentsSearchFilter />
               {isAnyFilterModified && (
@@ -43,6 +45,8 @@ export const AgentsListHeader = () => {
                 </Button>
               )}
             </div>
+            <ScrollBar orientation="horizontal" />
+            </ScrollArea>
         </div>
       </>
     );
