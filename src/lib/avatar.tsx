468dfc1beb705d0a7c8a03d2ab7ae4c2c015f759
amriteshx1 <amriteshx18@gmail.com>
2025-10-01 14:09:51 +0530

    fixed variant typo inside generateAvatarUri

--- src/lib/avatar.tsx
+++ src/lib/avatar.tsx
@@ -3,13 +3,13 @@ import { botttsNeutral, initials } from "@dicebear/collection";
 
 interface Props{
     seed: string;
-    variant: "bottsNeutral" | "initials";
+    variant: "botttsNeutral" | "initials";
 };
 
 export const generateAvatarUri = ({ seed, variant }: Props) => {
     let avatar;
 
-    if(variant === "bottsNeutral"){
+    if(variant === "botttsNeutral"){
         avatar = createAvatar(botttsNeutral, {seed});
     }else {
         avatar = createAvatar(initials, { seed, fontWeight: 500, fontSize: 42});
