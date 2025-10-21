623e7e32a2c8e2eec9e5c09f318c1cc147b2e520
amriteshx1 <amriteshx18@gmail.com>
2025-10-21 16:37:30 +0530

    updated call-ended to conditionally display message to free user's about auto-ended calls

--- src/modules/call/ui/components/call-ended.tsx
+++ src/modules/call/ui/components/call-ended.tsx
@@ -1,12 +1,27 @@
+import { useEffect, useState } from "react";
 import Link from "next/link";
+
+import { isFreeUser } from "@/lib/isFreeUser";
 
 import { Button } from "@/components/ui/button";
 
-export const CallEnded = () => {
+interface Props {
+  userId: string;
+}
+
+export const CallEnded = ({ userId }: Props) => {
+    const [isFree, setIsFree] = useState<boolean | null>(null);
+
+    useEffect(() => {
+      (async () => {
+        const free = await isFreeUser(userId);
+        setIsFree(free);
+      })();
+    }, [userId]);
 
     return (
         <div className="flex flex-col items-center justify-center h-full bg-radial from-sidebar-accent to-sidebar">
-            <div className="py-4 px-8 flex flex-1 items-center justify-center">
+            <div className="py-4 px-8 flex flex-col flex-1 items-center justify-center">
                 <div className="flex flex-col items-center justify-center gap-y-6 bg-background rounded-lg p-10 shadow-sm">
                     <div className="flex flex-col gap-y-2 text-center">
                         <h6 className="text-lg font-medium">You have ended the call</h6>
@@ -16,6 +31,14 @@ export const CallEnded = () => {
                         <Link href="/meetings">Back to meetings</Link>
                     </Button>
                 </div>
+                {isFree && (
+                  <p className="text-xs mt-5 text-white/70">Free call ended after a minute. Kindly{" "} 
+                      <Link href="/upgrade" className="text-white underline">
+                          Upgrade
+                      </Link>{" "}
+                      to enjoy longer sessions.
+                  </p>
+                )}
             </div>
         </div>
     );
